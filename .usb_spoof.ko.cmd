b7d981dacc0d3673fc9a932f43311ef168242b9d
Abhilasha Tandon <abhilashatandon167@gmail.com>
2026-02-13 22:06:21 -0500

    cleaned up directory

--- .usb_spoof.ko.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_usb_spoof.ko := ld -r -m elf_x86_64 -z noexecstack --no-warn-rwx-segments --build-id=sha1  -T /usr/src/linux-headers-6.17.0-14-generic/scripts/module.lds -o usb_spoof.ko usb_spoof.o usb_spoof.mod.o .module-common.o
--- .usb_spoof.mod.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_usb_spoof.mod := printf '%s\n'   usb_spoof.o | awk '!x[$$0]++ { print("./"$$0) }' > usb_spoof.mod

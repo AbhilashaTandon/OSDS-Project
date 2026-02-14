1edb492ac845353a21a00bac5d27ec637080d7a1
Abhilasha Tandon <abhilashatandon167@gmail.com>
2026-02-13 23:09:45 -0500

    added fops struct and function to add char device to system, but without fops functions

--- usb_spoof.c
+++ usb_spoof.c
@@ -1,34 +1,40 @@
-#include <linux/module.h> // needed by all modules
-#include <linux/printk.h> // needed for pr_info()
-#include <linux/init.h> // needed for macros
-#include <linux/cdev.h> 
-#define DRIVER_NAME "usb_spoofer"
-
-static struct cdev USB_cdev;
-static int USB_major_no;
-static int USB_minor_no;
-
+#include "usb_spoof.h"
 
 static int __init usb_spoof_init(void){
 	pr_info("Hello, world!\n");
 
-	dev_t dev;
 	int ret;
 
-	ret = alloc_chrdev_region(&dev, 0, 1, DRIVER_NAME);
+	ret = alloc_chrdev_region(&USB_dev, 0, 1, DRIVER_NAME);
 
 	if(ret){ return ret; }
 
-	USB_major_no = MAJOR(dev);
-	USB_minor_no = MINOR(dev);
+	USB_major_no = MAJOR(USB_dev);
+	USB_minor_no = MINOR(USB_dev);
+
+	cdev_init(&USB_cdev, &fops);
+
+	ret = cdev_add(&USB_cdev, USB_dev, 1);
+
+	if(ret){
+	       	printk(KERN_ALERT "error with adding char device to system: %d.\n", ret); 
+		return ret;
+       	}
+	
 
 	pr_info("I'm %s. I'm a kernel module with major number %d and minor number %d.\n", DRIVER_NAME, USB_major_no, USB_minor_no);	
+	printk(KERN_INFO "To talk to\n");
+	printk(KERN_INFO "the driver, create a dev file with\n");
+	printk(KERN_INFO "'mknod /dev/%s c %d 0'.\n", DRIVER_NAME, USB_major_no);
+	printk(KERN_INFO "Try various minor numbers. Try to cat and echo to\n");
+	printk(KERN_INFO "the device file.\n");
+	printk(KERN_INFO "Remove the device file and module when done.\n");
 
-	unregister_chrdev_region(dev, 0);
 
 	return 0;
 }
 static void __exit usb_spoof_exit(void){
+	unregister_chrdev_region(USB_dev, 0);
 }
 
 module_init(usb_spoof_init);

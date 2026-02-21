f3381adfe46a0e7cbb324155a7607895f0942573
Abhilasha Tandon <abhilashatandon167@gmail.com>
2026-02-21 14:12:01 -0500

    Consolidated global data into struct, but need to update code to use it

--- src/gaomon_driver.c
+++ src/gaomon_driver.c
@@ -1,14 +1,14 @@
 #include "gaomon_driver.h"
 #include <linux/usb.h>
 
-#define VENDOR_ID  0x256c
+#define VENDOR_ID 0x256c
 #define PRODUCT_ID 0x006e
 #define MINOR_BASE 192
 
 // gaomon: vendorid=256c productid=006e
 
 static struct usb_device_id gaomon_id_table[] = {
-	{USB_DEVICE(VENDOR_ID, PRODUCT_ID) },
+	{USB_DEVICE(VENDOR_ID, PRODUCT_ID)},
 	{},
 };
 
@@ -19,36 +19,50 @@ static struct file_operations gaomon_fops = {
 };
 
 static struct usb_class_driver gaomon_class_driver = {
-	.name =		"gaomon%d",
-	.fops =		&gaomon_fops,
-	.minor_base =	MINOR_BASE,
+	.name = "gaomon%d",
+	.fops = &gaomon_fops,
+	.minor_base = MINOR_BASE,
 };
 
 MODULE_DEVICE_TABLE(usb, gaomon_id_table);
 
-static int gaomon_probe(struct usb_interface *intf, const struct usb_device_id *id){
-	//second argument points to entry in my_id_table
+static int gaomon_probe(struct usb_interface *intf, const struct usb_device_id *id)
+{
+	// second argument points to entry in my_id_table
+	struct gaomon_data *data;
+
+	data = kzalloc(sizeof(*dev), GFP_KERNEL);
+	if (!data)
+	{
+		return -ENOMEM;
+	}
+
 	printk(KERN_INFO "%s - usb probe function\n", DRIVER_NAME);
 
-	int error_code;
+	data->udev = usb_get_dev(interface_to_usbdev(intf));
+	data->uintf = usb_get_intf(intf);
+
+		int error_code;
 
 	error_code = usb_register_dev(intf, &gaomon_class_driver);
 
-	if(error_code) {
+	if (error_code)
+	{
 		printk(KERN_ALERT "Unable to register this driver. Not able to get a minor for this device. Error code %d\n", error_code);
 		return error_code;
 	}
 
 	dev_info(&intf->dev,
-			"USB device now attached to USB-%d",
-			intf->minor);
+			 "USB device now attached to USB-%d",
+			 intf->minor);
 
 	return error_code;
 
 	return 0;
 }
 
-static void gaomon_disconnect(struct usb_interface *intf){
+static void gaomon_disconnect(struct usb_interface *intf)
+{
 	printk(KERN_INFO "%s - usb disconnect function\n", DRIVER_NAME);
 	usb_deregister_dev(intf, &gaomon_class_driver);
 }
@@ -60,16 +74,20 @@ static struct usb_driver gaomon_driver = {
 	.disconnect = gaomon_disconnect,
 };
 
-static ssize_t gaomon_read(struct file *file, char *buffer, size_t count, loff_t *ppos){
-	if(buffer != NULL){
-		for(int i = 0; i < count; i++){
+static ssize_t gaomon_read(struct file *file, char *buffer, size_t count, loff_t *ppos)
+{
+	if (buffer != NULL)
+	{
+		for (int i = 0; i < count; i++)
+		{
 			printk(KERN_INFO "%02x", buffer + i);
 		}
 	}
 	return 0;
 }
 
-static ssize_t gaomon_write(struct file *filp, const char __user *buff,size_t len, loff_t *off){
+static ssize_t gaomon_write(struct file *filp, const char __user *buff, size_t len, loff_t *off)
+{
 	pr_alert("Sorry, this operation is not supported.\n");
 	return -EINVAL;
 }
@@ -103,24 +121,26 @@ static int device_release(struct inode *inode, struct file *file)
 /* Called when a process, which already opened the dev file, attempts to
  * read from it.
  */
-static ssize_t device_read(struct file *filp, /* see include/linux/fs.h */
-		char __user *buffer, /* buffer to fill with data */
-		size_t length, /* length of the buffer */
-		loff_t *offset)
+static ssize_t device_read(struct file *filp,	/* see include/linux/fs.h */
+						   char __user *buffer, /* buffer to fill with data */
+						   size_t length,		/* length of the buffer */
+						   loff_t *offset)
 {
 	/* Number of bytes actually written to the buffer */
 	int bytes_read = 0;
 	const char *msg_ptr = msg;
 
-	if (!*(msg_ptr + *offset)) { /* we are at the end of message */
+	if (!*(msg_ptr + *offset))
+	{				 /* we are at the end of message */
 		*offset = 0; /* reset the offset */
-		return 0; /* signify end of file */
+		return 0;	 /* signify end of file */
 	}
 
 	msg_ptr += *offset;
 
 	/* Actually put the data into the buffer */
-	while (length && *msg_ptr) {
+	while (length && *msg_ptr)
+	{
 		/* The buffer is in the user data segment, not the kernel
 		 * segment so "*" assignment won't work. We have to use
 		 * put_user which copies data from the kernel data segment to
@@ -137,11 +157,13 @@ static ssize_t device_read(struct file *filp, /* see include/linux/fs.h */
 	return bytes_read;
 }
 
-static int __init gaomon_driver_init(void){
+static int __init gaomon_driver_init(void)
+{
 	printk(KERN_INFO "%s - Hello!", DRIVER_NAME);
 
 	int retval;
-	if ((retval = alloc_chrdev_region(&gaomon_device, MINOR_BASE, 1,DRIVER_NAME)) < 0)	{
+	if ((retval = alloc_chrdev_region(&gaomon_device, MINOR_BASE, 1, DRIVER_NAME)) < 0)
+	{
 		printk(KERN_ALERT "%s - Error during allocating region. Error code %d.\n", DRIVER_NAME, retval);
 		return retval;
 	}
@@ -149,33 +171,37 @@ static int __init gaomon_driver_init(void){
 	gaomon_major_no = MAJOR(gaomon_device);
 	gaomon_minor_no = MINOR(gaomon_device);
 
-	pr_info("I'm %s. I'm a kernel module with major number %d and minor number %d.\n", DRIVER_NAME, gaomon_major_no, gaomon_minor_no);	
+	pr_info("I'm %s. I'm a kernel module with major number %d and minor number %d.\n", DRIVER_NAME, gaomon_major_no, gaomon_minor_no);
 	printk(KERN_INFO "This line is a test. To talk to\n");
 	printk(KERN_INFO "the driver, create a dev file with\n");
-	printk(KERN_INFO "'mknod /dev/%s c %d %d'.\n", DRIVER_NAME, gaomon_major_no, gaomon_minor_no); printk(KERN_INFO "Try various minor numbers. Try to cat and echo to\n"); printk(KERN_INFO "the device file.\n");
+	printk(KERN_INFO "'mknod /dev/%s c %d %d'.\n", DRIVER_NAME, gaomon_major_no, gaomon_minor_no);
+	printk(KERN_INFO "Try various minor numbers. Try to cat and echo to\n");
+	printk(KERN_INFO "the device file.\n");
 	printk(KERN_INFO "Remove the device file and module when done.\n");
 
 	cdev_init(&gaomon_char_device, &gaomon_fops);
 	retval = cdev_add(&gaomon_char_device, gaomon_device, 1);
 
-	if(retval){
+	if (retval)
+	{
 		printk(KERN_ALERT "Error registering char driver.\n");
 		return retval;
 	}
 
 	retval = usb_register(&gaomon_driver);
 
-	if(retval){
+	if (retval)
+	{
 		printk(KERN_ALERT "%s - Error during register. Error code %d.\n", DRIVER_NAME, retval);
 		return retval;
 	}
 
-	printk(KERN_INFO "%s - USB driver registered successfully!.\n", DRIVER_NAME); 
+	printk(KERN_INFO "%s - USB driver registered successfully!.\n", DRIVER_NAME);
 	return 0;
 }
 
-
-static void __exit gaomon_driver_exit(void){
+static void __exit gaomon_driver_exit(void)
+{
 	printk(KERN_INFO "%s - Goodbye!", DRIVER_NAME);
 
 	usb_deregister(&gaomon_driver);
--- src/gaomon_driver.h
+++ src/gaomon_driver.h
@@ -9,9 +9,9 @@
 #include <linux/device.h>
 #include <linux/errno.h>
 #include <linux/fs.h>
-#include <linux/init.h> // needed for macros
+#include <linux/init.h>	  // needed for macros
 #include <linux/kernel.h> /* for sprintf() */
-#include <linux/kref.h> //kernel ref counter
+#include <linux/kref.h>	  //kernel ref counter
 #include <linux/module.h> // needed by all modules
 #include <linux/mutex.h>
 #include <linux/printk.h> // needed for pr_info()
@@ -23,34 +23,32 @@
 #define DRIVER_NAME "gaomon_driver"
 #define BUF_LEN 80 /* Max length of the message from the device */
 
-static unsigned int gaomon_device;
-static struct cdev gaomon_char_device;
+struct gaomon_data
+{
+	struct usb_device *udev;
+	struct usb_interface uintf;
+	unsigned char *buffer;
+	size_t buffer_size = 4096;
+	size_t buffer_usage = 0;
+	static unsigned int device_no;
+	static struct cdev cdev;
+	static int major_no;
+	static int minor_no;
+};
+
+// fops methods
 
 static int gaomon_open(struct inode *, struct file *);
 static int gaomon_release(struct inode *, struct file *);
 static ssize_t gaomon_read(struct file *, char __user *, size_t, loff_t *);
 static ssize_t gaomon_write(struct file *, const char __user *, size_t, loff_t *);
 
+// usb methods
+
 static int gaomon_probe(struct usb_interface *intf, const struct usb_device_id *id);
 static void gaomon_disconnect(struct usb_interface *intf);
-static ssize_t gaomon_read(struct file *file, char *buffer, size_t count, loff_t *ppos);
 
 static int __init gaomon_driver_init(void);
 static void __exit gaomon_driver_exit(void);
-
-static struct class *gaomon_class;
-
-static int gaomon_major_no; /* major number assigned to our device driver */
-static int gaomon_minor_no; /* minor number assigned to our device driver */
-
-enum {
-	CDEV_NOT_USED,
-	CDEV_EXCLUSIVE_OPEN,
-};
-
-/* Is device open? Used to prevent multiple access to device */
-static atomic_t already_open = ATOMIC_INIT(CDEV_NOT_USED);
-
-static char msg[BUF_LEN + 1]; /* The msg the device will give when asked */
 
 #endif

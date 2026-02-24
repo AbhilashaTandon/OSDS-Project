f9833704ad6822481f3a2df899987bd791b39b1d
Abhilasha Tandon <abhilashatandon167@gmail.com>
2026-02-23 23:50:53 -0500

    added lines to autoremove mod before loading and autoremove/create dev file

--- src/see_mod_output.sh
+++ src/see_mod_output.sh
@@ -1,4 +1,7 @@
 sudo modprobe -r hid_uclogic
 sudo modprobe -r usbhid
+sudo rmmod gaomon_driver
+sudo rm gaomon_driver
 sudo insmod gaomon_driver.ko
+sudo mknod gaomon_driver c 239 192
 sudo dmesg | grep gaomon_driver

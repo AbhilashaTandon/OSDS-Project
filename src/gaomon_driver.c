f9c505b47f5d8c8a5a97c20bf64462faa21ea26a
Abhilasha Tandon <abhilashatandon167@gmail.com>
2026-02-23 23:34:28 -0500

    removed unused code and updated read function

--- src/gaomon_driver.c
+++ src/gaomon_driver.c
@@ -61,12 +61,25 @@ static struct usb_driver gaomon_driver = {
 };
 
 static ssize_t gaomon_read(struct file *file, char *buffer, size_t count, loff_t *ppos){
-	if(buffer != NULL){
-		for(int i = 0; i < count; i++){
-			printk(KERN_INFO "%02x", buffer + i);
-		}
+	if(buffer == NULL){
+		return 0;
 	}
-	return 0;
+
+	unsigned char *user_buffer = kzalloc(count, GFP_KERNEL);
+
+	if(copy_to_user(buffer, user_buffer, count)){
+		printk(KERN_ALERT "%s - Error: not enough memory to read from USB", DRIVER_NAME);
+		kfree(user_buffer);
+		return -EFAULT;
+	}
+	else{
+		for(unsigned int i = 0; i < (count < 256 ? count : 256); i++){
+			printk(KERN_INFO "%s - %d: %d", DRIVER_NAME, i, *(user_buffer + i));
+		}
+
+		kfree(user_buffer);
+		return count;
+	}
 }
 
 static ssize_t gaomon_write(struct file *filp, const char __user *buff,size_t len, loff_t *off){
@@ -74,68 +87,6 @@ static ssize_t gaomon_write(struct file *filp, const char __user *buff,size_t le
 	return -EINVAL;
 }
 
-/* Methods */
-
-/* Called when a process tries to open the device file, like
- * "sudo cat /dev/chardev"
- */
-static int device_open(struct inode *inode, struct file *file)
-{
-	static int counter = 0;
-
-	if (atomic_cmpxchg(&already_open, CDEV_NOT_USED, CDEV_EXCLUSIVE_OPEN))
-		return -EBUSY;
-
-	sprintf(msg, "I already told you %d times Hello world!\n", counter++);
-
-	return 0;
-}
-
-/* Called when a process closes the device file. */
-static int device_release(struct inode *inode, struct file *file)
-{
-	/* We're now ready for our next caller */
-	atomic_set(&already_open, CDEV_NOT_USED);
-
-	return 0;
-}
-
-/* Called when a process, which already opened the dev file, attempts to
- * read from it.
- */
-static ssize_t device_read(struct file *filp, /* see include/linux/fs.h */
-		char __user *buffer, /* buffer to fill with data */
-		size_t length, /* length of the buffer */
-		loff_t *offset)
-{
-	/* Number of bytes actually written to the buffer */
-	int bytes_read = 0;
-	const char *msg_ptr = msg;
-
-	if (!*(msg_ptr + *offset)) { /* we are at the end of message */
-		*offset = 0; /* reset the offset */
-		return 0; /* signify end of file */
-	}
-
-	msg_ptr += *offset;
-
-	/* Actually put the data into the buffer */
-	while (length && *msg_ptr) {
-		/* The buffer is in the user data segment, not the kernel
-		 * segment so "*" assignment won't work. We have to use
-		 * put_user which copies data from the kernel data segment to
-		 * the user data segment.
-		 */
-		put_user(*(msg_ptr++), buffer++);
-		length--;
-		bytes_read++;
-	}
-
-	*offset += bytes_read;
-
-	/* Most read functions return the number of bytes put into the buffer. */
-	return bytes_read;
-}
 
 static int __init gaomon_driver_init(void){
 	printk(KERN_INFO "%s - Hello!", DRIVER_NAME);

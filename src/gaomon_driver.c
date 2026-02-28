8838e9c37b6d8b7f23aed2118a479caa2bf0da7d
Abhilasha Tandon <abhilashatandon167@gmail.com>
2026-02-28 18:14:10 -0500

    removed device file deletion and creation from auto run script

--- src/gaomon_driver.c
+++ src/gaomon_driver.c
@@ -37,6 +37,7 @@ static int gaomon_probe(struct usb_interface *intf, const struct usb_device_id *
 	if (!data->urb) {
 		return -ENOMEM;
 	}
+	data->buffer_usage = 0;
 
 	/* save our data pointer in this interface device */
 	usb_set_intfdata(intf, data);
--- src/see_mod_output.sh
+++ src/see_mod_output.sh
@@ -3,8 +3,6 @@ make
 sudo modprobe -r hid_uclogic
 sudo modprobe -r usbhid
 sudo rmmod gaomon_driver
-sudo rm gaomon_driver
 sudo insmod gaomon_driver.ko
-sudo mknod gaomon_driver c 239 192
 sudo dmesg | grep gaomon_driver
 cat gaomon_driver

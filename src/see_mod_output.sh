4f894638fe8d33ecba3c05e32a1d07c1ab71a18d
Abhilasha Tandon <abhilashatandon167@gmail.com>
2026-03-03 22:51:14 -0500

    Added deleting and recreating dev file to shell script

--- src/see_mod_output.sh
+++ src/see_mod_output.sh
@@ -4,5 +4,7 @@ sudo modprobe -r hid_uclogic
 sudo modprobe -r usbhid
 sudo rmmod gaomon_driver
 sudo insmod gaomon_driver.ko
+sudo rm /dev/gaomon_driver
+sudo mknod /dev/gaomon_driver c 239 0
 cat /dev/gaomon_driver > log.txt
 sudo dmesg | grep gaomon_driver
